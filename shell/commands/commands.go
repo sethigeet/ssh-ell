bcab00ad78fca3c4db40ed844b6b9c97c8ccf30a
Geet Sethi <geetsethi05@gmail.com>
2022-01-07 16:58:53 +0530

    Implemented the logic for the `help` command

--- shell/commands/commands.go
+++ shell/commands/commands.go
@@ -13,40 +13,40 @@ import (
 
 // Commands is the list of the commands supported by the shell package
 var Commands = []prompt.Suggest{
+	{Text: "connect", Description: "Connect using the set options"},
+	{Text: "download", Description: "Download a file from the connected server"},
+	{Text: "get", Description: "Get the value of an option"},
 	{Text: "help", Description: "Print out help info"},
 	{Text: "set", Description: "Set the value of an option"},
-	{Text: "get", Description: "Get the value of an option"},
-	{Text: "connect", Description: "Connect using the set options"},
 	{Text: "upload", Description: "Upload a file to the connected server"},
-	{Text: "download", Description: "Download a file from the connected server"},
 }
 
 // Options is the list of options supported by the `set` and `get` commands
 // which set and get the options required by other commands
 var Options = []prompt.Suggest{
+	{Text: "authMethod", Description: "The auth method you want to use to connect to the host"},
 	{Text: "host", Description: "The host you want to connect to"},
 	{Text: "port", Description: "The port of the host to which you want to connect to"},
-	{Text: "user", Description: "The user on the host to which you want to connect to"},
 	{Text: "timeout", Description: "The timeout for the ssh connection"},
-	{Text: "authMethod", Description: "The auth method you want to use to connect to the host"},
+	{Text: "user", Description: "The user on the host to which you want to connect to"},
 }
 
 var conn ssh.Connection
 
 func Complete(d prompt.Document) []prompt.Suggest {
 	switch strings.Split(d.TextBeforeCursor(), " ")[0] {
+	case "connect":
+		return (Connect{}).Complete(d)
+	case "download":
+		return (Download{}).Complete(d)
+	case "get":
+		return (Get{}).Complete(d)
 	case "help":
 		return (Help{}).Complete(d)
 	case "set":
 		return (Set{}).Complete(d)
-	case "get":
-		return (Get{}).Complete(d)
-	case "connect":
-		return (Connect{}).Complete(d)
 	case "upload":
 		return (Upload{}).Complete(d)
-	case "download":
-		return (Download{}).Complete(d)
 	}
 
 	return prompt.FilterFuzzy(Commands, d.GetWordBeforeCursor(), true)
@@ -55,17 +55,17 @@ func Complete(d prompt.Document) []prompt.Suggest {
 func Execute(s string) {
 	cmd := strings.Split(s, " ")
 	switch cmd[0] {
+	case "connect":
+		(Help{}).Execute(cmd[1:]...)
+	case "download":
+		(Help{}).Execute(cmd[1:]...)
+	case "get":
+		(Help{}).Execute(cmd[1:]...)
 	case "help":
 		(Help{}).Execute(cmd[1:]...)
 	case "set":
 		(Help{}).Execute(cmd[1:]...)
-	case "get":
-		(Help{}).Execute(cmd[1:]...)
-	case "connect":
-		(Help{}).Execute(cmd[1:]...)
 	case "upload":
-		(Help{}).Execute(cmd[1:]...)
-	case "download":
 		(Help{}).Execute(cmd[1:]...)
 	default:
 		color.New(color.FgRed, color.Bold).Fprintln(os.Stderr, "Invalid command specified")
--- shell/commands/help.go
+++ shell/commands/help.go
@@ -1,12 +1,61 @@
 package commands
 
-import "github.com/c-bata/go-prompt"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/c-bata/go-prompt"
+	"github.com/fatih/color"
+)
 
 type Help struct{}
 
+var helpMessages = map[string]string{
+	"ssh-ell": `Write a generic help message for ssh-ell here`,
+
+	"connect":  `Write a help message for the connect command here`,
+	"download": `Write a help message for the download command here`,
+	"get":      `Write a help message for the get command here`,
+	"help":     `Write a help message for the help command here`,
+	"set":      `Write a help message for the set command here`,
+	"upload":   `Write a help message for the upload command here`,
+}
+
 func (Help) Complete(d prompt.Document) []prompt.Suggest {
-	return []prompt.Suggest{}
+	cmd := strings.Split(d.TextBeforeCursor(), " ")
+	if len(cmd) > 2 {
+		return nil
+	}
+
+	return Commands
 }
 
 func (Help) Execute(args ...string) {
+	if len(args) > 1 {
+		color.New(color.FgRed, color.Bold).Fprintln(os.Stderr, "Invalid args specified")
+		color.New(color.FgRed).Fprintln(os.Stderr, "The help command can only take one arg at max!")
+		return
+	}
+
+	var cmd string
+	if len(args) == 0 {
+		cmd = "ssh-ell"
+	} else {
+		cmd = args[0]
+	}
+
+	helpMessage, exists := helpMessages[cmd]
+
+	if !exists {
+		color.New(color.FgRed, color.Bold).Fprintln(os.Stderr, "Invalid command specified")
+		color.New(color.FgRed).Fprintln(os.Stderr, "Please ask for help for a command that exists!")
+		return
+	}
+
+	header := fmt.Sprintf("Help for %s:", cmd)
+	color.New(color.FgGreen, color.Bold).Println(header)
+	log := color.New(color.FgGreen)
+	log.Println(strings.Repeat("-", len(header)))
+	log.Println(helpMessage)
 }
